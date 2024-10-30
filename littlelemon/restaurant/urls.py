6cbcb1fadff1ae0ef0c2b5c111267697823d53b6
glen-app <glenturley@yahoo.co.uk>
2024-10-30 09:48:28 +0000

    lemon4

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin 
 from django.urls import path 
-from . import views
+from .views import MenuItemsView, SingleMenuItemView
 
 urlpatterns = [
-    path('', views.index, name='index')
+    #path('', views.index, name='index')
+    path('menu/', MenuItemsView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),
 ]
